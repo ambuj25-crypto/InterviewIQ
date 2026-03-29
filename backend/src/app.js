88bacca13c017599fdb68a6588fe02662657c156
ambuj25-crypto <mambuj04@gmail.com>
2026-03-29 17:27:44 +0530

    Refactor CORS origin configuration to array format

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,8 +7,8 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
-     "https://interviewiq-2507.vercel.app",
+    origin: ["http://localhost:5173",
+     "https://interviewiq-2507.vercel.app"],
     credentials: true
 }))
 
